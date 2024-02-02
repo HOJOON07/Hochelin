dc70d0d61ee00c128009a73f7526eb1eae8298bd
hojoon <ghwns107@naver.com>
2024-02-02 14:13:01 +0900

    페이지네이션

--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -1,15 +1,28 @@
 import { PrismaClient } from "@prisma/client";
-import { StoreType } from "./../../interface/index";
+import { StoreApiResonpse, StoreType } from "./../../interface/index";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<StoreType[]>
+  res: NextApiResponse<StoreApiResonpse>
 ) {
   const prisma = new PrismaClient();
+
+  const count = await prisma.store.count();
+  const { page = "1" }: { page?: string } = req.query;
+  const skipPage = parseInt(page) - 1;
   const stores = await prisma.store.findMany({
     orderBy: { id: "asc" },
+    take: 10,
+    skip: skipPage * 10,
   });
 
-  res.status(200).json(stores);
+  // totalpage, data, page, totalCount
+
+  res.status(200).json({
+    page: parseInt(page),
+    data: stores,
+    totalCount: count,
+    totalPage: Math.ceil(count / 10),
+  });
 }
