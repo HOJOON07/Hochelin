74d010f8fcc6fdedcb4a729c8deae31b9db2eb16
hojoon <ghwns107@naver.com>
2024-02-25 23:23:52 +0900

    디버그 스크립트 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -47,3 +47,16 @@ export default function App({ Component, pageProps }: AppProps) {
 //     },
 //   },
 // });
+
+// const [queryClient] = React.useState(
+//   () =>
+//     new QueryClient({
+//       defaultOptions: {
+//         queries: {
+//           // With SSR, we usually want to set some default staleTime
+//           // above 0 to avoid refetching immediately on the client
+//           staleTime: 60 * 1000,
+//         },
+//       },
+//     }),
+// )
