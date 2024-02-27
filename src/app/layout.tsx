2f59f75a09826d046db373ed786b5b6bfdf70bd1
hojoon <ghwns107@naver.com>
2024-02-27 23:12:02 +0900

    google analytics 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 
 import { NextLayout, NextProvider } from "./provider";
 import { Metadata } from "next";
+import GoogleAnalytics from "./googleAnalytics";
 
 export const metadata: Metadata = {
   title: "Hochelin",
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <GoogleAnalytics GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_ID} />
         <NextProvider>
           <NextLayout>{children}</NextLayout>
         </NextProvider>
